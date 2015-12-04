a327f0f16c6cedf3bda0ea6c89bf345741f16a87
乐天 <13913364179@163.com>
2015-12-04 17:49:53 +0800

    状态机

--- src/main/java/com/test/patterns/state/s3/Test.java
+++ src/main/java/com/test/patterns/state/s3/Test.java
@@ -5,11 +5,15 @@ public class Test {
 	public static void main(String[] args) {
 		ContextState context = new ContextState();
 		
-		context.cancel();
+		//context.cancel();
 		
 		context.pay();
 		
 		context.packaging();
+		
+		context.stowage();
+		
+		context.confirm();
 
 	}
 
